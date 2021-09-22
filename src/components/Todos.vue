45332f6defc7c648fd067c9e5afa30ede8178cb8
Guillaume <leo.guillaume@epitech.eu>
2021-09-22 19:42:23 +0200

    task to do and completed state style

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -10,36 +10,44 @@
 
     <div class="main">
       <ul class="todo-list">
-        <li class="todo" v-for="todo in todos" :key="todo.name">
-           <div class="view">
-             <input type="checkbox" v-model="todo.completed" class="toggle">
-             <label>{{todo.name}}</label>
-           </div>
+        <li class="todo" v-for="todo in todos" :key="todo.name" :class="{completed: todo.completed}">
+          <div class="view">
+            <input type="checkbox" v-model="todo.completed" class="toggle">
+            <label>{{ todo.name }}</label>
+          </div>
         </li>
       </ul>
     </div>
+    <footer class="footer">
+      <span class="todo-count"><strong>{{remaining}}</strong> tâches à faire</span>
+    </footer>
   </section>
 </template>
 
 <script>
 export default {
   name: "Todos",
-  data () {
+  data() {
     return {
-        todos : [{
-          name: "Tache de test",
-          completed: false
-        }],
-        newTodo: ''
+      todos: [{
+        name: "Tache de test",
+        completed: true
+      }],
+      newTodo: ''
     }
   },
   methods: {
-    addTodo(){
+    addTodo() {
       this.todos.push({
         name: this.newTodo,
         completed: false
       })
       this.newTodo = ''
+    }
+  },
+  computed: {
+    remaining(){
+      return this.todos.filter(todo => !todo.completed).length
     }
   }
 }
