bb8d15a4c4f282472eaf65738ce7a6de6b17dec5
Guillaume <leo.guillaume@epitech.eu>
2021-09-22 22:11:51 +0200

    deleted all completed todos

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -21,13 +21,18 @@
         </li>
       </ul>
     </div>
-    <footer class="footer">
+    <footer class="footer" v-show="todos.length > 0">
       <span class="todo-count"><strong>{{remaining}}</strong> tâches à faire</span>
       <ul class="filters">
         <li><a href="#" :class="{selected: filter === 'all'}" @click.prevent="filter = 'all'">Toutes</a></li>
         <li><a href="#" :class="{selected: filter === 'todo'}" @click.prevent="filter = 'todo'">A faire</a></li>
         <li><a href="#" :class="{selected: filter === 'done'}" @click.prevent="filter = 'done'">Faites</a></li>
       </ul>
+      <button class="clear-completed"
+              v-show="doneTodos"
+              @click="deleteCompleted">
+        Supprimer les tâches finies
+      </button>
     </footer>
   </section>
 </template>
@@ -38,10 +43,7 @@ export default {
   data() {
     return {
       allDone: true,
-      todos: [{
-        name: "Tache de test",
-        completed: true
-      }],
+      todos: [],
       newTodo: '',
       filter: 'all'
     }
@@ -56,6 +58,9 @@ export default {
     },
     deleteTodo(todo) {
       this.todos = this.todos.filter(e => e !== todo)
+    },
+    deleteCompleted() {
+      this.todos = this.todos.filter(todo => !todo.completed)
     }
   },
   computed: {
@@ -69,6 +74,12 @@ export default {
           todo.completed = value;
         })
       }
+    },
+    doneTodos(){
+      return this.getTodosDone.length > 0
+    },
+    getTodosDone(){
+      return this.todos.filter(todo => todo.completed)
     },
     remaining(){
       return this.todos.filter(todo => !todo.completed).length
